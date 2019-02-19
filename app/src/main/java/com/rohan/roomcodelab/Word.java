07793831de1703a93c1410c42f4a7939c5fa8f35
Rohan <rohan_dce@outlook.com>
2019-02-19 21:54:07 +0530

    Word class added

--- app/src/main/java/com/rohan/roomcodelab/Word.java
+++ app/src/main/java/com/rohan/roomcodelab/Word.java
@@ -2,9 +2,20 @@ package com.rohan.roomcodelab;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
 
 @Entity(tableName = "word table")
 public class Word {
+    @PrimaryKey
+    @NonNull
     @ColumnInfo(name = "word")
     private String Word;
+
+    public Word(String word){this.Word=word;}
+
+    @NonNull
+    public String getWord() {
+        return Word;
+    }
 }
