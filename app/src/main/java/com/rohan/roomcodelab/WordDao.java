d4e424c32abd1efbac420ff177a47a71ee8b9c78
Rohan <rohan_dce@outlook.com>
2019-02-19 22:49:21 +0530

    Added LiveData to Word Dao

--- app/src/main/java/com/rohan/roomcodelab/WordDao.java
+++ app/src/main/java/com/rohan/roomcodelab/WordDao.java
@@ -1,8 +1,11 @@
 package com.rohan.roomcodelab;
 
+import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface WordDao {
@@ -14,5 +17,5 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * FROM word_table ORDER BY word ASC")
-    void display();
+    LiveData<List<Word>> display();
 }
